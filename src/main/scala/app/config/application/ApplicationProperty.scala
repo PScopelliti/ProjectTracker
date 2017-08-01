3c170ef83f5762bed2688ba5094a4ebe008e29ea
Giuseppe Scopelliti <Giuseppe.Scopelliti@telegraph.co.uk>
2017-08-01 16:29:28 +0100

    Added app properties

--- src/main/scala/app/config/application/ApplicationProperty.scala
+++ src/main/scala/app/config/application/ApplicationProperty.scala
@@ -4,80 +4,12 @@ import app.config.ConfigurationLoader
 
 trait ApplicationProperty {
 
-  /**
-   * Endpoints for fetching the channels, items and packages from reuters.
-   *
-   * @return
-   */
-  def reutersBaseUriEndpoint: String
+  self: ConfigurationLoader =>
 
-  /**
-   * Endpoint for fetching the authentication token for Reuters
-   *
-   * @return
-   */
-  def reutersAuthenticationUriEndpoint: String
+  def applicationProperty: ApplicationProperty = new ApplicationProperty
 
-  /**
-   * The common path contained in the Api path
-   *
-   * @return
-   */
-  def reutersCommonPathUri: String
-
-  /**
-   * The application port
-   *
-   * @return
-   */
-  def reutersApplicationPort: String
-
-  /**
-   * The username used for authenticate against Reuters APIs
-   *
-   * @return
-   */
-  def reutersApplicationUsername: String
-
-  /**
-   * The password used for authenticate against Reuters APIs
-   *
-   * @return
-   */
-  def reutersApplicationPassword: String
-
-}
-
-object ApplicationPropertyImpl extends ApplicationProperty with ConfigurationLoader {
-
-  /**
-   * @inheritdoc
-   */
-  override def reutersBaseUriEndpoint: String = configuration.getString("app.uri.endpoint.base")
-
-  /**
-   * @inheritdoc
-   */
-  override def reutersAuthenticationUriEndpoint: String = configuration.getString("app.uri.endpoint.authentication")
-
-  /**
-   * @inheritdoc
-   */
-  override def reutersCommonPathUri: String = configuration.getString("app.uri.path.common")
-
-  /**
-   * @inheritdoc
-   */
-  override def reutersApplicationPort: String = configuration.getString("app.port")
-
-  /**
-   * @inheritdoc
-   */
-  override def reutersApplicationUsername: String = configuration.getString("app.username")
-
-  /**
-   * @inheritdoc
-   */
-  override def reutersApplicationPassword: String = configuration.getString("app.password")
+  class ApplicationProperty {
+    def applicationPort: String = configuration.getString("app.port")
+  }
 
 }
--- src/main/scala/app/config/datastore/RedisDBProperty.scala
+++ src/main/scala/app/config/datastore/RedisDBProperty.scala
@@ -4,30 +4,16 @@ import app.config.ConfigurationLoader
 
 trait RedisDBProperty {
 
-  /**
-   * Contains the name of the dynamo db table with settings.
-   *
-   * @return tableName
-   */
-  def settingsTableName: String
+  self: ConfigurationLoader =>
 
-  /**
-   * Contains the key of the dynamo db table with settings.
-   *
-   * @return
-   */
-  def settingsTableKey: String
-}
+  val dbProperties = new RedisDBProperty
 
-object RedisDBPropertyImpl extends RedisDBProperty with ConfigurationLoader {
+  class RedisDBProperty {
 
-  /**
-   * @inheritdoc
-   */
-  override def settingsTableName: String = configuration.getString("dynamodb.table.settings.name")
+    def settingsTableName: String = configuration.getString("dynamodb.table.settings.name")
 
-  /**
-   * @inheritdoc
-   */
-  override def settingsTableKey: String = configuration.getString("dynamodb.table.settings.key")
+
+    def settingsTableKey: String = configuration.getString("dynamodb.table.settings.key")
+  }
+
 }
